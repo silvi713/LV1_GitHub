1952062253784d3bcd6c3182350eec4fe5627cf3
SilvioJavorek <silvio.javorek@vuv.hr>
2021-03-31 11:20:57 +0200

    treci commit

--- js/app.js
+++ js/app.js
@@ -7,10 +7,13 @@ var table=document.getElementById("tablicaPodataka");
   var cell2 = row.insertCell(1);
   var cell3 = row.insertCell(2);
   var cell4 = row.insertCell(3);
+  var cell5 = row.insertCell(4);
+  
 cell1.innerHTML = document.getElementById("lPredmet").value;
 cell2.innerHTML = document.getElementById("lsemestar").value;
 cell3.innerHTML = document.getElementById("lEctsbodovi").value;
-cell4.innerHTML = '<button onclick="SomeDeleteRowFunction(this)"><img class="slike" src="images/delete.png"></button> ';
+cell4.innerHTML = '<button onclick="SomeDeleteRowFunction(this)" class="fas fa-minus"></button> ';
+cell5.innerHTML = '<input type="checkbox" id="myCheck" onclick="selectedRow()">';
 
 }
 function SomeDeleteRowFunction(rows)
@@ -18,3 +21,29 @@ function SomeDeleteRowFunction(rows)
 var _row = rows.parentElement.parentElement;
   document.getElementById('tablicaPodataka').deleteRow(_row.rowIndex);
 }
+
+
+function selectedRow(){
+
+                var index,
+                    table = document.getElementById("tablicaPodataka");
+
+                for(var i = 1; i < table.rows.length; i++)
+                {
+                    table.rows[i].onclick = function()
+                    {
+                         // remove the background from the previous selected row
+                        if(typeof index !== "undefined"){
+                           table.rows[index].classList.toggle("selected");
+                        }
+                        console.log(typeof index);
+                        // get the selected row index
+                        index = this.rowIndex;
+                        // add class selected to the row
+                        this.classList.toggle("selected");
+                        console.log(typeof index);
+                     };
+                }
+
+            }
+            selectedRow();

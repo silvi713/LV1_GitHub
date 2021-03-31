2aec27d1ad48d89c0114f6a56cef992a4129b7da
silvi713 <silvio.javorek@vsmti.hr>
2021-03-31 18:39:55 +0200

    treci commit

--- js/app.js
+++ js/app.js
@@ -2,48 +2,61 @@ function DodajPodatke()
 {
 
 var table=document.getElementById("tablicaPodataka");
-  var row = table.insertRow(0);
+  var row = table.insertRow(1);
   var cell1 = row.insertCell(0);
   var cell2 = row.insertCell(1);
   var cell3 = row.insertCell(2);
-  var cell4 = row.insertCell(3);
-  var cell5 = row.insertCell(4);
+  var cell4= row.insertCell(3);
+  var cell5= row.insertCell(4);
   
 cell1.innerHTML = document.getElementById("lPredmet").value;
 cell2.innerHTML = document.getElementById("lsemestar").value;
 cell3.innerHTML = document.getElementById("lEctsbodovi").value;
-cell4.innerHTML = '<button onclick="SomeDeleteRowFunction(this)" class="fas fa-minus"></button> ';
+cell4.innerHTML = '<button onclick="ObrisiRed(this)" class="fas fa-minus"></button> ';
 cell5.innerHTML = '<input type="checkbox" id="myCheck" onclick="selectedRow()">';
 
 }
-function SomeDeleteRowFunction(rows)
+
+
+function ObrisiRed(rows)
 {
 var _row = rows.parentElement.parentElement;
   document.getElementById('tablicaPodataka').deleteRow(_row.rowIndex);
 }
 
 
-function selectedRow(){
+function selectedRow()
+{
+  var table = document.getElementById('tablicaPodataka');
 
-                var index,
-                    table = document.getElementById("tablicaPodataka");
+  table.addEventListener("click", function(ev){
+    if (ev.target.tagName == "INPUT") {
+      if (ev.target.checked) {
+        ev.target.parentNode.parentNode.classList.add("selected");
+      }else {
+        ev.target.parentNode.parentNode.classList.remove("selected");
+      }
+    }
+  });
+}
 
-                for(var i = 1; i < table.rows.length; i++)
-                {
-                    table.rows[i].onclick = function()
-                    {
-                         // remove the background from the previous selected row
-                        if(typeof index !== "undefined"){
-                           table.rows[index].classList.toggle("selected");
-                        }
-                        console.log(typeof index);
-                        // get the selected row index
-                        index = this.rowIndex;
-                        // add class selected to the row
-                        this.classList.toggle("selected");
-                        console.log(typeof index);
-                     };
-                }
 
-            }
-            selectedRow();
+
+function Pretrazivanje() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("tablicaPodataka");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }       
+    }
+  }
